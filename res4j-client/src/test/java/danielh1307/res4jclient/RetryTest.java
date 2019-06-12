c2604aba2cc8b8dd58cf1759fa05daf5cd885d1e
Daniel Hamm <daniel.hamm@sanitas.com>
2019-06-12 11:20:51 +0200

    Added tests.

---
+++ res4j-client/src/test/java/danielh1307/res4jclient/RetryTest.java
@@ -0,0 +1,87 @@
+package danielh1307.res4jclient;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.NestedServletException;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@AutoConfigureMockMvc
+public class RetryTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private RestTemplate restTemplate;
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void success_no_retry() throws Exception {
+        ResponseEntity<String> okEntity = new ResponseEntity<>("Hello", HttpStatus.OK);
+        when(restTemplate.getForEntity(anyString(), any(Class.class))).thenReturn(okEntity);
+
+        this.mockMvc.perform(get("/res4jclient/host-working"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello Welt\n"));
+
+        verify(this.restTemplate, times(1)).getForEntity(anyString(), any(Class.class));
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void failed_four_retries() {
+        when(restTemplate.getForEntity(anyString(), any(Class.class))).thenThrow(new RuntimeException());
+
+        try {
+            this.mockMvc.perform(get("/res4jclient/host-working"));
+        } catch (NestedServletException ex) {
+            if (!(ex.getCause() instanceof RuntimeException)) {
+                Assert.fail("Unexpected excpetion thrown");
+            }
+        } catch (Exception ex) {
+            Assert.fail("Unexpected exception thrown");
+        }
+
+        verify(this.restTemplate, times(4)).getForEntity(anyString(), any(Class.class));
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void failed_with_404_no_retries() {
+        when(restTemplate.getForEntity(anyString(), any(Class.class))).thenThrow(new HttpClientErrorException(NOT_FOUND));
+
+        try {
+            this.mockMvc.perform(get("/res4jclient/host-working"));
+        } catch (NestedServletException ex) {
+            if (!(ex.getCause() instanceof RuntimeException)) {
+                Assert.fail("Unexpected excpetion thrown");
+            }
+        } catch (Exception ex) {
+            Assert.fail("Unexpected exception thrown");
+        }
+
+        verify(this.restTemplate, times(1)).getForEntity(anyString(), any(Class.class));
+    }
+
+}
